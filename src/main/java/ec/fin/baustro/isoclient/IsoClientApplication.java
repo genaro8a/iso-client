de2391d981b66fce0cfa4ce1b2e4d812e5f98579
Saúl Ochoa <genarosaul@gmail.com>
2024-11-08 17:02:22 -0500

    Initial commit

---
+++ src/main/java/ec/fin/baustro/isoclient/IsoClientApplication.java
@@ -0,0 +1,13 @@
+package ec.fin.baustro.isoclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsoClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsoClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/fin/baustro/isoclient/IsoClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.fin.baustro.isoclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsoClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
